7f1073c66d80a1d78d1d2fa3f01a5c62c2ba9eac
dhanshreea <DhanshreeA@users.noreply.github.com>
2022-12-22 10:16:01 +0530

    Add grow_mol mode from crem into run_generate api

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -5,7 +5,7 @@ import sys
 from typing import List
 
 from rdkit import Chem
-from crem.crem import mutate_mol
+from crem.crem import mutate_mol, grow_mol
 
 # parse arguments
 input_file = sys.argv[1]
@@ -14,20 +14,24 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# checkpoints directory
+# Path to ChEMBL dataset with fragments of SC score 2
 database_dir = os.path.abspath(os.path.join(root, "..", "..", "databases"))
-
-# read checkpoints (here, simply an integer number: 42)
 database_path = os.path.join(database_dir, "replacements02_sc2.db")
 
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
+# Generate unique molecules with replacement fragments from the database
 def my_model(smiles_list: List[str], database_path: str) -> List[List[str]]:
-    generated_smiles = list()
+    output_smiles = list()
     input_mols = [Chem.MolFromSmiles(smi) for smi in smiles_list]
     for mol in input_mols:
-        generated_smiles.append(list(mutate_mol(mol=mol, db_name=database_path)))
+        try:
+            mutation_result = list(mutate_mol(mol=mol, db_name=database_path))
+            growth_result = list(grow_mol(mol=mol, db_name=database_path))
+            generated_smiles = list(set(mutation_result+growth_result)) # Keep unique smiles
+        except KeyError:
+            generated_smiles = list(set(mutation_result)) # Ignore grow_mol result if it runs into an error
+        output_smiles.append(generated_smiles)
     
-    return generated_smiles
+    return output_smiles
     
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -41,6 +45,6 @@ outputs = my_model(smiles_list, database_path)
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
+    writer.writerow(["generated_molecules"]) # header
     for o in outputs:
         writer.writerow(o)
